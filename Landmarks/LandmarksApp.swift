7992b2a59a5deadaded66aadd39ca95b2e6b21e3
Harry-KNIGHT <elliotharryk@gmail.com>
2021-10-03 12:50:19 +0200

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Elliot knight on 03/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
